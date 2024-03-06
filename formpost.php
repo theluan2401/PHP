ea2aa7d70152fd351e255a01fd653da9a1602504
theluan2401 <suluan24012002@gmail.com>
2024-03-06 16:24:37 +0700

    commit

--- formpost.php
+++ formpost.php
@@ -1,4 +1,14 @@
-<?php
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta> charset ="utf-8">
+        <title>Form Get</title>
+    </head>
+<body>
+    <h1>Save to file</h1>
+    <a href="form.html">Back to Form</a> <br />
+    <?php
 // Lấy dữ liệu từ POST
 $name = $_POST['name'];
 $email = $_POST['email'];
@@ -10,4 +20,13 @@ $email = $_POST['email'];
 echo "<h2>Cảm ơn bạn đã gửi thông tin!</h2>";
 echo "<p>Tên: " . $name . "</p>";
 echo "<p>Email: " . $email . "</p>";
+
+//save to file
+$output = $name . " # " . $email . "\n";
+$file = fopen("formdata.txt", "ab");
+fwrite($file, $output);
+fclose($file);
+echo "Data writen to file !!!";
 ?>
+</body>
+</html>
